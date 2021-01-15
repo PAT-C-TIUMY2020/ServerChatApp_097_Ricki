0a845bfd3b21ac20eb8cb40d4823704547baf2bb
Ricki Irawan <ricki.irawan.ft18@mail.umy.ac.id>
2021-01-15 19:24:04 +0700

    Add project files.

---
+++ ServerChatApp_097_Ricki Irawan/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_097;
+
+namespace ServerChatApp_097_Ricki_Irawan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(ServiceCallback));
+                hostObj.Open();
+
+                Console.WriteLine("Server ON, Ready to use");
+                Console.ReadLine();
+
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
